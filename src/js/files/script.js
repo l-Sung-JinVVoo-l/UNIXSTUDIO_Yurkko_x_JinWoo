cba5f16540d68d8e3e2ad54859d894a9f7c100a6
Yura Serkiz <serkiz1040@gmail.com>
2023-11-24 00:07:23 +0200

    chenges

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -4,3 +4,14 @@ import { isMobile } from './functions.js';
 import { flsModules } from './modules.js';
 
 // Wrap every letter in a span
+
+const headerBtmBorder = document.querySelector('.header__bottom-border');
+
+window.addEventListener('scroll', (e) => {
+  if (window.scrollY > 1) {
+    headerBtmBorder.classList.add('active-scroll');
+  }
+  if (window.scrollY < 1) {
+    headerBtmBorder.classList.remove('active-scroll');
+  }
+});
